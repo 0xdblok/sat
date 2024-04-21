b6f219b5c34d99b7884710e039e422bbf3098090
0xdblok <dblok0x@gmail.com>
2024-04-21 17:31:55 +0200

    ee

--- app/component/nav.js
+++ app/component/nav.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Nav() {
   return (
     <div>
-      <div className="flex md:px-16 px-2  md:pt-8 w-full">
+      <div className="flex md:px-16 px-2  pt-2 md:pt-8 w-full">
         {" "}
         <Image
           className=""
           src="https://fatherofbitcoin.xyz/images/8c713eeb4ade0a7227e5a06076084117.svg"
           alt=""
-          width={56}
-          height={56}
+          width={36}
+          height={36}
         />
       </div>
     </div>
