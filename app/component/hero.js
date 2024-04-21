bc0704db15f4b8f49488092d5886da905b11a719
0xdblok <dblok0x@gmail.com>
2024-04-21 19:24:53 +0200

    ee

--- app/component/hero.js
+++ app/component/hero.js
@@ -57,7 +57,7 @@ export default function Hero() {
             invention, Bitcoin, in the day of the halving.
           </p>
           <div className="flex justify-center ">
-            <Image className="" src={sol} alt="" width={40} height={40} />
+            <Image className="" src={sol} alt="" width={100} height={100} />
           </div>{" "}
         </div>
       </div>
