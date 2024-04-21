1cc4eafe3f00245d298ac6fb9dd5cddd5deed884
0xdblok <dblok0x@gmail.com>
2024-04-21 17:35:06 +0200

    ee

--- app/component/nav.js
+++ app/component/nav.js
@@ -6,7 +6,7 @@ export default function Nav() {
       <div className="flex md:px-16 px-2  pt-2 md:pt-8 w-full">
         {" "}
         <Image
-          className="md:w-[56px] h-[56px]"
+          className="md:w-[56px] md:h-[56px]"
           src="https://fatherofbitcoin.xyz/images/8c713eeb4ade0a7227e5a06076084117.svg"
           alt=""
           width={36}
